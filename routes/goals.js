810b830d667bf1874ff4906600673b5a27ea77e2
Kenneth Truong <ket032@ucsd.edu>
2018-03-01 02:54:32 -0800

    Lastest

--- routes/goals.js
+++ routes/goals.js
@@ -1,8 +1,18 @@
 var data = require("../goals.json");
 
+
 exports.viewGoals = function(req, res) { 
   var name = req.params.name; 
   res.render("goals", data);
+
 }
 
+ /* const fs = require('fs');
+	var m = JSON.parse(fs.readFileSync('goals.json').toString());
+	Object.keys(m).forEach(function(p){
+    p.description = m.description;
+	});
+	fs.writeFile('goals.json', JSON.stringify(m));
 
+
+*/
--- routes/homepage.js
+++ routes/homepage.js
@@ -1,8 +1,19 @@
 
 var data = require("../test.json");
 
+var quotes = [
+'i am',
+'a quote',
+'yes'
+]
+
+function newQuote() {
+	var randomNumber = Math.floor(Math.random() * (quotes.length));
+	document.getElementbyId('quoteDisplay').innerHTML = quotes[randomNumber];
+
+}
+
 exports.viewHomepage = function(req, res){
-  console.log(data);
   var name = req.params.name; 
 	res.render('homepage')
 };
