6154fff02aeed6a10efd5bc14e8053d2e1b1b853
Kenneth Truong <ket032@ucsd.edu>
2018-03-07 01:36:42 -0800

    exercise html problem

--- routes/excercises.js
+++ routes/excercises.js
@@ -1,11 +1,5 @@
 var data2 = require("../test.json");
 
 exports.viewExcercises = function(req, res) { 
-  data2["viewAlt"] = false;
   res.render("excercise", data2);
-}
-
-exports.viewAlt = function(req,res){
-	data2["viewAlt"] = true;
-	res.render("excercise", data2);
 };
