80505c672e79f947d503abcb75b6217759634c4d
Kenneth Truong <ket032@ucsd.edu>
2018-03-07 01:03:16 -0800

    randomize excercise page

--- routes/excercises.js
+++ routes/excercises.js
@@ -1,16 +1,15 @@
 var data = require("../test.json");
 
 exports.viewExcercises = function(req, res) { 
-  console.log(data);
+  data["viewAlt"] = false;
   var name = req.params.name; 
   res.render("excercise", data);
 }
 
-function initializePage() {
-	// your code here
-	$(".data{{id}}").click(function(){
-		ga("send", "event", "collapse", "click");
-	});
-}
+exports.viewAlt = function(req,res){
+	data["viewAlt"] = true;
+	res.render('excercise', data);
+};
+
 
 
